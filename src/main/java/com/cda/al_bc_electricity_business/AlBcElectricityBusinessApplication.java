7506eb9ccd7877aa2dd19d4830ed60cbe1eac02c
aLouis Home <alouis@stagiaire-humanbooster.com>
2025-08-01 13:24:57 +0200

    Initial commit: project setup with pom.xml and basic Spring Boot structure

---
+++ src/main/java/com/cda/al_bc_electricity_business/AlBcElectricityBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.cda.al_bc_electricity_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlBcElectricityBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlBcElectricityBusinessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cda/al_bc_electricity_business/AlBcElectricityBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cda.al_bc_electricity_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlBcElectricityBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
